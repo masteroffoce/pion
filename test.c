e731d5d4d0f7037cc82c5f7883bad71c6627d2b4
Calcium <masteroffoce@gmail.com>
2025-06-21 21:00:45 +0200

    Once again, backup

--- test.c
+++ test.c
@@ -5,8 +5,9 @@ int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
 
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Safe GTK Layer Shell");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 50);
+    gtk_window_set_title(GTK_WINDOW(window), "GTK Layer Shell Popup");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 150);
+    gtk_widget_set_size_request(window, 300, 150);
 
     gtk_layer_init_for_window(GTK_WINDOW(window));
     gtk_layer_set_layer(GTK_WINDOW(window), GTK_LAYER_SHELL_LAYER_TOP);
@@ -14,13 +15,15 @@ int main(int argc, char **argv) {
     gtk_layer_set_exclusive_zone(GTK_WINDOW(window), 0);
     gtk_layer_set_anchor(GTK_WINDOW(window),
                          GTK_LAYER_SHELL_EDGE_TOP |
-                         GTK_LAYER_SHELL_EDGE_LEFT |
-                         GTK_LAYER_SHELL_EDGE_RIGHT,
+                         GTK_LAYER_SHELL_EDGE_LEFT,
                          TRUE);
+    gtk_layer_set_margin(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_TOP, 200);
 
-    GtkWidget *button = gtk_button_new_with_label("Hello, world!");
-    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_add(GTK_CONTAINER(window), button);
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    GtkWidget *label = gtk_label_new("Firefox\nKitty");
+    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
@@ -30,3 +33,7 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+gboolean key_pressed(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
+    gtk_main_quit();
+    return TRUE;
+}
